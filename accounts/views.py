2b6061e55af50a1d2b336bedaeaf224b72ec6c34
Thennal A P <thennalap.91@gmail.com>
2025-12-03 21:57:08 +0400

    Vendor Dashboard Editing

--- accounts/views.py
+++ accounts/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.decorators import login_required,user_passes_test
 from django.core.exceptions import PermissionDenied
 from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
+from vendor.models import Vendor
 
 
 #restrict the vendor from accessing the customer pages
@@ -166,7 +167,7 @@ def customerDashboard(request):
 
 @login_required(login_url='login')
 @user_passes_test(check_role_vendor)
-def vendorDashboard(request):
+def vendorDashboard(request):    
     return render(request,'accounts/vendorDashboard.html')
 
 def forgotPassword(request):
--- vendor/models.py
+++ vendor/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from accounts.models import User,UserProfile
+from accounts.utils import send_notification
 
 # Create your models here.
 class Vendor(models.Model):
@@ -13,4 +14,26 @@ class Vendor(models.Model):
 
     def __str__(self):
         return self.vendor_name
+    
+    def save(self,*args,**kwargs):
+        if self.pk is not None:
+            # This is an update...
+            original=Vendor.objects.get(pk=self.pk)
+            if original.is_approved != self.is_approved:
+                mail_template='accounts/emails/admin_approval_email.html'
+                context={
+                    "user":self.user,
+                    "is_approved":self.is_approved,
+                    "vendor_name":self.vendor_name
+
+                }
+                if self.is_approved == True:
+                    # send notification email
+                    mail_subject = "Congratulations! Your restaurant has been approved"              
+                    send_notification(mail_subject,mail_template,context)
+                else:
+                    # Send notification email
+                    mail_subject = "We're sorry — Your restaurant has not been approved"                 
+                    send_notification(mail_subject,mail_template,context)
+        return super(Vendor,self).save(*args,**kwargs)
 
